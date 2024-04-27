9b5e223da856ca910cf6d8ef5930fd990e40e279
Ryan Bowler <slouchh07@gmail.com>
2024-04-27 11:28:48 -0700

    Create for loop to draw grid - Added for loop to draw 16x16 grid - Added event listener on each square that changes   the background colour when moused over

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
+// Draw the 16 X 16 grid
 const container = document.querySelector("#container");
+
 for(let i = 0; i < 256; i++) {
   const square = document.createElement("div");
-  square.className = "pixel-block";
+  square.className = `pixel-${i}`;
   container.appendChild(square);
+
+  // Event listener for each square
+  square.addEventListener("mouseover", event => {
+  event.target.style.backgroundColor = "black";
+  console.log("mouseover");
+});
 }
+
+
+
