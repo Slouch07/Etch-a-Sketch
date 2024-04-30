94269fa944e1eef330b8b65f322f9d7185dc98de
Ryan Bowler <slouchh07@gmail.com>
2024-04-29 19:08:54 -0700

    Add logic to grid sizing button - Included an if/else statement for when a user   inputs a number less than zero and more than 100 - Added a clearGrid function to the button for when   the user selects a new grid size. - Put the main code for drawing the board into a   drawGrid function and placed it in the event   listener

--- javascript.js
+++ javascript.js
@@ -1,36 +1,42 @@
 const container = document.querySelector("#container");
 
-// Draw default 16 x 16 grid
+// Draw grid function
+function drawGrid(gridSize) {
+  for (let i = 1; i <= gridSize; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+    // Draw pixels (squares) within each row
+    for (let j = 1; j <= gridSize; j++) {
+      const pixel = document.createElement("div");
+      pixel.classList.add("pixel");
+      pixel.style.width = "100%";
+      pixel.style.height = 800 / gridSize;
+      row.appendChild(pixel);
+      // Event listener for each pixel
+      pixel.addEventListener("mouseover", event => {
+        event.target.style.backgroundColor = "black";
+      });
+    }
+  };
+}
 
-// Draw rows
-for (let row = 1; row <= 16; row++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-  container.appendChild(row);
-  // Draw pixels (squares) within each row
-  for (let pixel = 1; pixel <= 16; pixel++) {
-    const pixel = document.createElement("div");
-    pixel.classList.add("pixel");
-    pixel.style.width = "100%";
-    pixel.style.height = 800 / 16;
-    row.appendChild(pixel);
-    // Event listener for each pixel
-    pixel.addEventListener("mouseover", event => {
-      event.target.style.backgroundColor = "black";
-    });
+function clearGrid() {
+  while (container.firstChild) {
+    container.firstChild.remove();
   }
-};
+}
 
 // Event listener for grid size button
 const sizeButton = document.querySelector(".size-button");
 sizeButton.addEventListener("click", event => {
-  let gridSize = prompt("Select a grid size from 0 to 100: ");
-});
-
-// Event listener for reset button
-const resetButton = document.querySelector(".reset-button");
-resetButton.addEventListener("click", event => {
-  location.reload();
+  clearGrid();
+  let size = prompt("Select a grid size from 1 to 100: ");
+  if (size <= 0 || size > 100) {
+    alert("Please enter a number between 1 and 100.");
+  } else {
+    drawGrid(size);
+  }
 });
 
 
